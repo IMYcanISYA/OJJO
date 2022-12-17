8144019d3675a4870e38ad02d8f06d71255a6b39
Oleksandr Zhukovskyi <117286726+IMYcanISYA@users.noreply.github.com>
2022-12-17 13:49:45 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('.form-block__input');
+
+input.addEventListener('change', () => {
+	if (input.value !== "") {
+		input.classList.add('_active');
+	} else input.classList.remove('_active');
+})
+
+const burgerBtn = document.querySelector(".burger-btn");
+const burgerMenu = document.querySelector(".nav-menu__list");
+
+burgerBtn.addEventListener("click", () => {
+	burgerMenu.classList.toggle("_active");
+	burgerBtn.classList.toggle("_active");
+	document.body.classList.toggle('_lock');
+})
---
+++ js/search.js
@@ -0,0 +1,23 @@
+
+const filterBtn = document.querySelectorAll('.filter-main__btn');
+
+filterBtn.forEach(item => {
+	item.addEventListener('click', () => {
+		filterBtn.forEach(e => {
+			if (item !== e) {
+				e.parentElement.classList.remove('_active');
+			};			
+		});
+		if (item.parentElement.classList.contains('_active')) {
+			item.parentElement.classList.remove("_active");
+		} else item.parentElement.classList.add("_active");
+	})
+})
+const burgerBtn = document.querySelector(".burger-btn");
+const burgerMenu = document.querySelector(".nav-menu__list");
+
+burgerBtn.addEventListener("click", () => {
+	burgerMenu.classList.toggle("_active");
+	burgerBtn.classList.toggle("_active");
+	document.body.classList.toggle('_lock');
+})
